cf06590565b3d15a3e342b1306ca36bf1b8a45e7
William Graf <7930703+willgraf@users.noreply.github.com>
2018-07-12 13:03:25 -0700

    should work with python3 and copied deepcell functions over to remove dependency

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Use the nvidia tensorflow:18.04-py3 image as the parent image
+FROM nvcr.io/vvlab/tensorflow:18.04-py3
+
+# System maintenance
+RUN apt update && apt-get install -y python3-tk
+RUN pip install --upgrade pip
+
+# Set working directory
+WORKDIR /data-engineering
+
+# Copy the requirements.txt and install the dependencies
+COPY requirements.txt /data-engineering
+RUN pip install -r /data-engineering/requirements.txt
+
+# Copy the annotation tools
+COPY annotation_scripts /data-engineering/annotation_scripts
+
+# Change matplotlibrc file to use the Agg backend
+RUN echo "backend : Agg" > /usr/local/lib/python3.5/dist-packages/matplotlib/mpl-data/matplotlibrc
--- annotation_scripts/convert_movies_to_training_data.py
+++ annotation_scripts/convert_movies_to_training_data.py
@@ -6,28 +6,13 @@ Inputs a segmented and tracked movie and then converts it to training data forma
 @author: David Van Valen
 """
 
-"""
-Import packages
-"""
-import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
-matplotlib.get_backend()
-import matplotlib.pyplot as plt
-import glob
 import os
+
+import numpy as np
 import skimage as sk
-import scipy as sp
-from scipy import ndimage
-from skimage import feature
-from sklearn.utils import class_weight
-from cnn_functions import get_images_from_directory
-from cnn_functions import format_coord as cf
-from skimage import morphology as morph
-import matplotlib.pyplot as plt
-from skimage.transform import resize
 from tensorflow.contrib.keras import backend as K
 
+from utils import get_images_from_directory
 
 """
 Load images
@@ -46,22 +31,21 @@ Construct and save semantic annotation for each frame
 """
 
 image_size_x, image_size_y = images[0].shape[2:]
-semantic_masks = np.zeros((1, len(images), image_size_x, image_size_y), dtype = K.floatx())
+semantic_masks = np.zeros((1, len(images), image_size_x, image_size_y), dtype=K.floatx())
 
 for time, image in enumerate(images):
-	image = image[0,0,:,:].astype('int')
-	edges = sk.segmentation.find_boundaries(image, mode = 'thick')
-	interior = 2*(image > 0)
-	semantic_mask = edges + interior
-	semantic_mask[semantic_mask == 3] = 1
+    image = image[0, 0, :, :].astype('int')
+    edges = sk.segmentation.find_boundaries(image, mode='thick')
+    interior = 2 * (image > 0)
+    semantic_mask = edges + interior
+    semantic_mask[semantic_mask == 3] = 1
 
-	# Swap category names - edges category 0, interior category 1, background category 2
-	semantic_mask_temp = np.zeros(semantic_mask.shape, dtype = 'int')
-	semantic_mask_temp[semantic_mask == 0] = 2 
-	semantic_mask_temp[semantic_mask == 1] = 0
-	semantic_mask_temp[semantic_mask == 2] = 1
+    # Swap category names - edges category 0, interior category 1, background category 2
+    semantic_mask_temp = np.zeros(semantic_mask.shape, dtype='int')
+    semantic_mask_temp[semantic_mask == 0] = 2
+    semantic_mask_temp[semantic_mask == 1] = 0
+    semantic_mask_temp[semantic_mask == 2] = 1
 
-	semantic_mask = semantic_mask_temp
-	file_name = os.path.join(save_direc,'HeLa_set2_frame_' + str(time) + '.png')
-	sk.io.imsave(file_name, semantic_mask)
-
+    semantic_mask = semantic_mask_temp
+    file_name = os.path.join(save_direc,'HeLa_set2_frame_' + str(time) + '.png')
+    sk.io.imsave(file_name, semantic_mask)
