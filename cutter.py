1c983ae4d75a4cd3a0309b5fd6de96226e0f1f58
NoraK10 <nora.e.koe@gmail.com>
2018-08-17 10:59:37 -0700

    new files

---
+++ cutter.py
@@ -0,0 +1,3 @@
+
+from post_annotation_scripts.cut_raw_segments import cut_raw
+cut_raw()
---
+++ move_to_movie.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+stacked_raw = input('path to stacked raw: ')
+destination = input('path to movie: ')
+raw_segments = os.listdir(stacked_raw)
+for segment in raw_segments:
+	i = segment.split('x_')[1][0]
+	j = segment.split('y_')[1][0]
+	files = os.listdir(os.path.join(stacked_raw, segment))
+	output_folder = os.path.join(destination, 'montage_'+str(i)+'_'+str(j), 'raw')
+	if os.path.isdir(output_folder) is False:
+		os.mkdir(output_folder)
+	for f in files:
+		new_name = ((f.split('slice_')[1]).split('.')[0]).zfill(2) + '.png'
+		shutil.copy(os.path.join(stacked_raw, segment, f), os.path.join(output_folder, new_name))
