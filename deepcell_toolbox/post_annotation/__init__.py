8a2779477c5c73cb6778d013c8c2d29475bba4e6
geneva-miller <geneva.elise.miller@gmail.com>
2019-04-23 10:05:46 -0700

    fix typo

--- deepcell_toolbox/post_annotation/__init__.py
+++ deepcell_toolbox/post_annotation/__init__.py
@@ -31,7 +31,7 @@ from __future__ import print_function
 from deepcell_toolbox.post_annotation import clean_montages
 from deepcell_toolbox.post_annotation import download_csv
 from deepcell_toolbox.post_annotation import montages_to_movies
-from deepcell_toolbox.post_annotation import overlapping_stitcher.py
+from deepcell_toolbox.post_annotation import overlapping_stitcher
 
 del absolute_import
 del division
