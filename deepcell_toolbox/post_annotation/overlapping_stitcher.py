bd1ad6fd6217c9845555bba683f8629cc3e2196b
geneva-miller <geneva.elise.miller@gmail.com>
2019-06-01 18:12:05 -0700

    add wrapper for stitcher, small changes to core stitcher
    -switch to using saved indices when applicable
    -use zeros if a sub image can't be loaded

--- deepcell_toolbox/post_annotation/overlapping_stitcher.py
+++ deepcell_toolbox/post_annotation/overlapping_stitcher.py
@@ -33,13 +33,21 @@ import numpy as np
 import os
 import sys
 import stat
-import warnings
 
-from skimage.io import imsave
+from imageio import imread, imwrite
 from deepcell_toolbox.utils.io_utils import get_image, get_img_names
-from deepcell_toolbox.utils.misc_utils import sorted_nicely
 
-def overlapping_stitcher(pieces_dir, save_dir, num_x, num_y, overlap_perc, identifier, sub_img_format, save_name):
+def overlapping_stitcher_core(pieces_dir, 
+                                 save_dir, 
+                                 x_start_indices, 
+                                 y_start_indices, 
+                                 overlapping_x_pix, 
+                                 overlapping_y_pix, 
+                                 original_x, 
+                                 original_y, 
+                                 identifier, 
+                                 sub_img_format, 
+                                 save_name):
 
     #save path
     img_path = os.path.join(save_dir, save_name)
@@ -54,26 +62,35 @@ def overlapping_stitcher(pieces_dir, save_dir, num_x, num_y, overlap_perc, ident
     small_img_size = test_img.shape
     
     #trim dimensions are the size of each sub image, with the overlap removed
-    trim_y = math.ceil(small_img_size[0]/(1+(2*overlap_perc/100)))
-    trim_x = math.ceil(small_img_size[1]/(1+(2*overlap_perc/100)))
-
-    #pad dimensions are the amount of padding added to a sub image on each edge    
-    pad_x = int((overlap_perc/100)*trim_x)
-    pad_y = int((overlap_perc/100)*trim_y)
+    trim_y = small_img_size[0] - 2*overlapping_y_pix
+    trim_x = small_img_size[1] - 2*overlapping_x_pix
     
-    full_y = trim_y*num_y
-    full_x = trim_x*num_x
+    #pad dimensions are the amount of padding added to a sub image on each edge
+    pad_x = overlapping_x_pix
+    pad_y = overlapping_y_pix
+    
+    full_y = original_y
+    full_x = original_x
     
     padded_full_img = np.zeros((full_y + 2*pad_y, full_x + 2*pad_x))
 
     #loop through the image pieces (sub_imgs)
-    for j in range(num_y):
-        for i in range(num_x):
+    for j in range(len(y_start_indices)):
+        for i in range(len(x_start_indices)):
 
             #load sub_img
             sub_img_name = sub_img_format.format(identifier,i,j)
             sub_img_path = os.path.join(pieces_dir, sub_img_name)
-            sub_img = get_image(sub_img_path)
+            try:
+                sub_img = get_image(sub_img_path)
+            except:
+                sub_img = np.zeros(small_img_size)
+            
+            sub_x = sub_img.shape[1]
+            sub_y = sub_img.shape[0]
+            
+            x_start = x_start_indices[i]
+            y_start = y_start_indices[j]
             
             #don't repeat values even though each sub image has similar values
             lowest_allowed_val = np.amax(padded_full_img)
@@ -83,12 +100,12 @@ def overlapping_stitcher(pieces_dir, save_dir, num_x, num_y, overlap_perc, ident
             #that value in the sub image, using most common value, keep other values in the sub image the same
             
             #working on top edge of sub_img
-            sub_img_overlap = sub_img[0:2*pad_y, 0:trim_x+2*pad_x]
-            full_img_overlap = padded_full_img[j*trim_y:j*trim_y+2*pad_y, i*trim_x:(i+1)*trim_x+2*pad_x]
+            sub_img_overlap = sub_img[0:2*pad_y, :]
+            full_img_overlap = padded_full_img[y_start:y_start+2*pad_y, x_start:x_start+sub_x]
             
             #add in cells from full_image that overlap with sub_img background
             merged_overlap = np.where(sub_img_overlap == 0, full_img_overlap, sub_img_overlap)
-            sub_img[0:2*pad_y, 0:trim_x+2*pad_x] = merged_overlap
+            sub_img[0:2*pad_y, :] = merged_overlap
             
             #get ids of cells in sub_img that have any pixels in this overlap region
             sub_cells = np.unique(merged_overlap)
@@ -110,12 +127,12 @@ def overlapping_stitcher(pieces_dir, save_dir, num_x, num_y, overlap_perc, ident
                     sub_img = np.where(sub_img == cell, overlapper, sub_img)
             
             #working on left edge of sub_img
-            sub_img_overlap = sub_img[0:trim_y+2*pad_y, 0:2*pad_x]
-            full_img_overlap = padded_full_img[j*trim_y:(j+1)*trim_y+2*pad_y, i*trim_x:i*trim_x+2*pad_x]
+            sub_img_overlap = sub_img[:, 0:2*pad_x]
+            full_img_overlap = padded_full_img[y_start:y_start+sub_y, x_start:x_start+2*pad_x]
             
             #add in cells from full_image that overlap with sub_img background
             merged_overlap = np.where(sub_img_overlap == 0, full_img_overlap, sub_img_overlap)
-            sub_img[0:trim_y+2*pad_y, 0:2*pad_x] = merged_overlap
+            sub_img[:, 0:2*pad_x] = merged_overlap
             
             #get ids of cells in sub_img that have any pixels in this overlap region
             sub_cells = np.unique(merged_overlap)
@@ -135,10 +152,8 @@ def overlapping_stitcher(pieces_dir, save_dir, num_x, num_y, overlap_perc, ident
                     #replaces "cell" values with the overlapper, leaves everything else unchanged
                     sub_img = np.where(sub_img == cell, overlapper, sub_img)
             
-            
             #put sub image into the full image
-            padded_full_img[int(j*trim_y):int(((j+1) * trim_y) + (2*pad_y)),
-                                             int(i*trim_x):int(((i+1) * trim_x) + (2*pad_x))] = sub_img
+            padded_full_img[y_start:y_start+sub_y, x_start:x_start+sub_x] = sub_img
             
     padded_full_img = padded_full_img.astype(np.uint16)
 
@@ -146,17 +161,109 @@ def overlapping_stitcher(pieces_dir, save_dir, num_x, num_y, overlap_perc, ident
     full_img = padded_full_img[pad_y:pad_y+full_y, pad_x:pad_x+full_x]
     
     #relabel cells so values aren't skipped
-    relabled_img = np.zeros(full_img.shape, dtype = np.uint16)
-    unique_cells = np.unique(full_img)
+    relabeled_img = np.zeros(full_img.shape, dtype = np.uint16)
+    unique_cells = np.unique(full_img) # get all unique values of y
     unique_cells = unique_cells[np.nonzero(unique_cells)]
     relabel_ids = np.arange(1, len(unique_cells) + 1)
 
     for cell_id, relabel_id in zip(unique_cells, relabel_ids):
-        relabled_img = np.where(full_img == cell_id, relabel_id, relabled_img)
-
+        relabeled_img = np.where(full_img == cell_id, relabel_id, relabeled_img)
 
     #save big image
-    #suppress "low contrast image" warnings
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        imsave(img_path, full_img)
+    imwrite(img_path, relabeled_img)
+
+def overlapping_stitcher_folder(pieces_dir, save_dir, identifier, num_images, json_chopper_log, is_2D):
+
+    #directories
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+        #add folder modification permissions to deal with files from file explorer
+        mode = stat.S_IRWXO | stat.S_IRWXU | stat.S_IRWXG
+        os.chmod(save_dir, mode)
+        
+    pieces_list = get_img_names(pieces_dir)
+        
+    #load test image for parameters if needed
+    test_img = imread(os.path.join(pieces_dir, pieces_list[0]))
+    small_img_size = test_img.shape
+    
+    #load variables from json log
+    overlap_perc = json_chopper_log['overlap_perc']
+    num_x_segments = json_chopper_log['num_x_segments']
+    num_y_segments = json_chopper_log['num_y_segments']
+    
+    try:
+        x_start_indices = json_chopper_log['x_start_indices']
+        y_start_indices = json_chopper_log['y_start_indices']
+        overlapping_x_pix = json_chopper_log['overlapping_x_pix']
+        overlapping_y_pix = json_chopper_log['overlapping_y_pix']
+        original_x = json_chopper_log['original_x']
+        original_y = json_chopper_log['original_y']
+        
+    #preferable to load these variables 
+    #but if they aren't in the log (outdated log or other reason?), calculate them    
+    except:
+    
+        #trim dimensions are the size of each sub image, with the overlap removed
+        trim_y = math.ceil(small_img_size[0]/(1+(2*overlap_perc/100)))
+        trim_x = math.ceil(small_img_size[1]/(1+(2*overlap_perc/100)))
+        
+        x_start_indices = []
+        for i in range(num_x_segments):
+            x_start_index = int(i*trim_x) 
+            x_start_indices.append(x_start_index)
+            
+        y_start_indices = []
+        for j in range(num_y_segments):
+            y_start_index = int(j*trim_y)
+            y_start_indices.append(y_start_index)
+        
+        overlapping_x_pix = (small_img_size[1] - trim_x) //2
+        overlapping_y_pix = (small_img_size[0] - trim_y) //2
+        #print('overlapping pix x, y', overlapping_x_pix, overlapping_y_pix)
+    
+        #not sure if overlapping_pix or pad is better calculation so leaving this in
+        #pad_x = int((overlap_perc/100)*trim_x)
+        #pad_y = int((overlap_perc/100)*trim_y)
+        #print('pad x, y', pad_x, pad_y)
+    
+        original_y = trim_y*num_y_segments
+        original_x = trim_x*num_x_segments
+    
+    #not all json logs with start indices have frame_offset info
+    try:
+        frame_offset = json_chopper_log['frame_offset']
+    except:
+        frame_offset = 0
+    
+    #construct image name format for all sub pieces of one image/frame
+    #also passes what the stitched image should be named to the stitcher core
+    for image in range(num_images):
+        
+        if is_2D:
+            sub_img_format = "_img_{0:03d}".format(image + frame_offset)
+
+            save_name = identifier + sub_img_format + "_annotation.png"       
+        
+            sub_img_format = "{0}" + sub_img_format + "_x_{1:02d}_y_{2:02d}_annotation.png"
+                        
+        else:
+            sub_img_format = "_frame_{0:03d}".format(image + frame_offset)
+    
+            save_name = identifier + sub_img_format + "_annotation.png"       
+        
+            sub_img_format = "{0}_x_{1:02d}_y_{2:02d}" + sub_img_format + "_annotation.png" 
+
+        print("Stitching image {0} of {1}".format(image + frame_offset + 1, num_images))
+        overlapping_stitcher_core(pieces_dir, 
+                                 save_dir, 
+                                 x_start_indices, 
+                                 y_start_indices, 
+                                 overlapping_x_pix, 
+                                 overlapping_y_pix, 
+                                 original_x, 
+                                 original_y, 
+                                 identifier, 
+                                 sub_img_format, 
+                                 save_name)
+    
