27452f34a7b7c4b1ee9112c17ff20256b3a9f543
geneva-miller <geneva.elise.miller@gmail.com>
2019-11-10 15:13:43 -0800

    add zstack prediction relabeling and all_frame relabeling
    - zstack prediction uses iou to relabel cells based on previous frame (adapted from Caliban function)
    - all_frame relabeling applies a regular relabeling scheme to each frame of an npz (good for 2D relabeling)
    - new relabeling options added to relabel_npz_folder

--- deepcell_toolbox/pre_annotation/npz_preprocessing.py
+++ deepcell_toolbox/pre_annotation/npz_preprocessing.py
@@ -270,6 +270,155 @@ def slice_npz_folder(src_folder, batch_size, save_dir):
     return None
 
 
+def predict_zstack_cell_ids(img, next_img, threshold = 0.1):
+    '''
+    Predict labels for next_img based on intersection over union (iou)
+    with img. If cells don't meet threshold for iou, they don't count as
+    matching enough to share label with "matching" cell in img. Cells
+    that don't have a match in img (new cells) get a new label so that
+    output relabeled_next does not skip label values (unless label values
+    present in prior image need to be skipped to avoid conflating labels).
+    '''
+
+    # relabel to remove skipped values, keeps subsequent predictions cleaner
+    relabeled_img = np.zeros(next_img.shape, next_img.dtype)
+
+    unique_cells = np.unique(next_img)
+    unique_cells = unique_cells[np.nonzero(unique_cells)]
+
+    # create array from starting value to starting value + number of cells to relabel
+    # ensures no labels are skipped
+    relabel_ids = np.arange(1, len(unique_cells) + 1)
+
+    # populate relabeled_annotations array with relabeled annotations for that frame
+    for cell_id, relabel_id in zip(unique_cells, relabel_ids):
+        relabeled_img = np.where(next_img == cell_id, relabel_id,
+                relabeled_img)
+
+    next_img = relabeled_img
+
+    #create np array that can hold all pairings between cells in one
+    #image and cells in next image
+    iou = np.zeros((np.max(img)+1, np.max(next_img)+1))
+
+    vals = np.unique(img)
+    cells = vals[np.nonzero(vals)]
+
+    #nothing to predict off of
+    if len(cells) == 0:
+        print('skipped')
+        return next_img
+
+    next_vals = np.unique(next_img)
+    next_cells = next_vals[np.nonzero(next_vals)]
+
+    #no values to reassign
+    if len(next_cells) == 0:
+        return next_img
+
+    #calculate IOUs
+    for i in cells:
+        for j in next_cells:
+            intersection = np.logical_and(img==i,next_img==j)
+            union = np.logical_or(img==i,next_img==j)
+            iou[i,j] = intersection.sum(axis=(0,1)) / union.sum(axis=(0,1))
+
+    #relabel cells appropriately
+
+    #relabeled_next holds cells as they get relabeled appropriately
+    relabeled_next = np.zeros(next_img.shape, dtype = np.uint16)
+
+    #max_indices[cell_from_next_img] -> cell from first image that matches it best
+    max_indices = np.argmax(iou, axis = 0)
+
+    #put cells that into new image if they've been matched with another cell
+
+    #keep track of which (next_img)cells don't have matches
+    #this can be if (next_img)cell matched background, or if (next_img)cell matched
+    #a cell already used
+    unmatched_cells = []
+    #don't reuse cells (if multiple cells in next_img match one particular cell)
+    used_cells_src = []
+
+    #next_cell ranges between 0 and max(next_img)
+    #matched_cell is which cell in img matched next_cell the best
+
+    # this for loop does the matching between cells
+    for next_cell, matched_cell in enumerate(max_indices):
+        #if more than one match, look for best match
+        #otherwise the first match gets linked together, not necessarily reproducible
+
+        # matched_cell != 0 prevents adding the background to used_cells_src
+        if matched_cell != 0 and matched_cell not in used_cells_src:
+            bool_matches = np.where(max_indices == matched_cell)
+            count_matches = np.count_nonzero(bool_matches)
+            if count_matches > 1:
+                #for a given cell in img, which next_cell has highest iou
+                matching_next_options = np.argmax(iou, axis =1)
+                best_matched_next = matching_next_options[matched_cell]
+
+                #ignore if best_matched_next is the background
+                if best_matched_next != 0:
+                    if next_cell != best_matched_next:
+                        unmatched_cells = np.append(unmatched_cells, next_cell)
+                        continue
+                    else:
+                        # don't add if bad match
+                        if iou[matched_cell][best_matched_next] > threshold:
+                            relabeled_next = np.where(next_img == best_matched_next, matched_cell, relabeled_next)
+
+                        # if it's a bad match, we still need to add next_cell back into relabeled next later
+                        elif iou[matched_cell][best_matched_next] <= threshold:
+                            unmatched_cells = np.append(unmatched_cells, best_matched_next)
+
+                        # in either case, we want to be done with the "matched_cell" from img
+                        used_cells_src = np.append(used_cells_src, matched_cell)
+
+            # matched_cell != 0 is still true
+            elif count_matches == 1:
+                #add the matched cell to the relabeled image
+                if iou[matched_cell][next_cell] > threshold:
+                    relabeled_next = np.where(next_img == next_cell, matched_cell, relabeled_next)
+                else:
+                    unmatched_cells = np.append(unmatched_cells, next_cell)
+
+                used_cells_src = np.append(used_cells_src, matched_cell)
+
+        elif matched_cell in used_cells_src and next_cell != 0:
+            #skip that pairing, add next_cell to unmatched_cells
+            unmatched_cells = np.append(unmatched_cells, next_cell)
+
+        #if the cell in next_img didn't match anything (and is not the background):
+        if matched_cell == 0 and next_cell !=0:
+            unmatched_cells = np.append(unmatched_cells, next_cell)
+            #note: this also puts skipped (nonexistent) labels into unmatched cells, main reason to relabel first
+
+    #figure out which labels we should use to label remaining, unmatched cells
+
+    #these are the values that have already been used in relabeled_next
+    relabeled_values = np.unique(relabeled_next)[np.nonzero(np.unique(relabeled_next))]
+
+    #to account for any new cells that appear, create labels by adding to the max number of cells
+    #assumes that these are new cells and that all prev labels have been assigned
+    #only make as many new labels as needed
+
+    current_max = max(np.max(cells), np.max(relabeled_values)) + 1
+
+    stringent_allowed = []
+    for additional_needed in range(len(unmatched_cells)):
+        stringent_allowed.append(current_max)
+        current_max += 1
+
+    #replace each unmatched cell with a value from the stringent_allowed list,
+    #add that relabeled cell to relabeled_next
+    if len(unmatched_cells) > 0:
+        for reassigned_cell in range(len(unmatched_cells)):
+            relabeled_next = np.where(next_img == unmatched_cells[reassigned_cell],
+                                 stringent_allowed[reassigned_cell], relabeled_next)
+
+    return relabeled_next
+
+
 def relabel_npz_unique(full_npz_path, start_val = 1):
     '''
     Load an npz and relabel each feature so that every cell in every
@@ -395,9 +544,173 @@ def relabel_npz_preserve_relationships(full_npz_path, start_val = 1):
     return None
 
 
-def relabel_npzs_folder(npz_dir, relabel_type = 'unique'):
+def relabel_npz_all_frames(full_npz_path, start_val = 1):
     '''
-    relabel each npz in a folder with a given relabling strategy
+    Relabels each frame in an npz starting from start_val. Each frame gets relabeled
+    independently of the other frames, so this can scramble relationship data if it
+    exists. Useful for cases where relationships do not need to be preserved (eg,
+    annotating, correcting, possibly reshaping npzs full of 2D annotations).
+
+    Npz should be 4D in shape (frames, y, x, channels).
+
+    Inputs:
+        full_npz_path: full path to npz to relabel. Used to load and then
+            save npz.
+        start_val: What label to begin relabeling with. 1 by default but
+            could be set to another value if needed for other purposes
+
+    Returns:
+        None (relabeled npzs are saved in place)
+    '''
+    # load npz
+    npz = np.load(full_npz_path)
+
+    # load raw and annotations, but we only need to modify annotations
+    raw = npz['X'][()]
+    annotations = npz['y'][()]
+
+    # TODO: make sure that npz is 4D
+
+    # assumes channels_last
+    features = annotations.shape[-1]
+
+    # create new array to store the relabeled annotations
+    relabeled_annotations = np.zeros(annotations.shape, dtype = annotations.dtype)
+
+    # features should be relabeled independently of each other
+    for f in range(features):
+
+        # relabel each frame
+        for frame in range(annotations.shape[0]):
+            img = annotations[frame,:,:,f]
+
+            # get all unique values in this frame, excluding 0 (background)
+            unique_cells = np.unique(img)
+            unique_cells = unique_cells[np.nonzero(unique_cells)]
+
+            # create array from starting value to starting value + number of cells to relabel
+            # ensures no labels are skipped
+            relabel_ids = np.arange(start_val, len(unique_cells) + start_val)
+
+            # populate relabeled_annotations array with relabeled annotations for that frame
+            for cell_id, relabel_id in zip(unique_cells, relabel_ids):
+                relabeled_annotations[frame,:,:,f] = np.where(img == cell_id, relabel_id,
+                    relabeled_annotations[frame,:,:,f])
+
+    # overwrite original npz with relabeled npz (only the annotations have changed)
+    np.savez(full_npz_path, X = raw, y = relabeled_annotations)
+
+    return None
+
+
+def relabel_npz_zstack_prediction(full_npz_path, start_val = 1, threshold = 0.1):
+    '''
+    Relabels the first frame of an npz starting from start_val. Each subsequent frame
+    gets relabeled based on iou (intersection over union) with the previous frame.
+    This can scramble human-annotated 3D label assignments if they exist--
+    use relabel_npz_preserve_relationships for relabeling human-corrected 3D labels.
+    Useful as a pre-processing step to reduce the human labor that goes into assigning labels.
+    This step should be applied when:
+        - launching a 3D npz that was created from unrelated 2D predictions
+        - the npz has not already been relabeled
+        - deep learning predictions are not robust for the data type yet
+
+    Note: the iou-based prediction was implemented for zstack prediction, but should
+    work okay for timelapse movies as well (although it is not expected to get divisions).
+    Deep learning tracking should be used to predict timelapse movies if the segmentations
+    are sufficiently good, otherwise this is an acceptable first pass.
+
+    Npz should be 4D in shape (frames, y, x, channels).
+
+    Inputs:
+        full_npz_path: full path to npz to relabel. Used to load and then
+            save npz.
+        start_val: What label to begin relabeling with. 1 by default but
+            could be set to another value if needed for other purposes
+        threshold: iou between cells that must be reached for zstack prediction to
+            consider them matched. 0.1 by default
+
+    Returns:
+        None (relabeled npzs are saved in place)
+    '''
+
+    # load npz
+    npz = np.load(full_npz_path)
+
+    # load raw and annotations, but we only need to modify annotations
+    raw = npz['X'][()]
+    annotations = npz['y'][()]
+
+    # TODO: make sure that npz is 4D
+
+    # assumes channels_last
+    features = annotations.shape[-1]
+
+    # create new array to store the relabeled annotations
+    relabeled_annotations = np.zeros(annotations.shape, dtype = annotations.dtype)
+
+    # features should be relabeled independently of each other
+    for f in range(features):
+
+        # relabel the first frame
+        # (even if this is empty, each next_frame gets relabeled as first step in
+        # predict_zstack_cell_ids, so whichever frame is the first to have labels
+        # will be relabeled from 1)
+
+        first_img = annotations[0,:,:,f]
+        unique_cells = np.unique(first_img)
+        unique_cells = unique_cells[np.nonzero(unique_cells)]
+
+        relabel_ids = np.arange(start_val, len(unique_cells) + start_val)
+        for cell_id, relabel_id in zip(unique_cells, relabel_ids):
+                        relabeled_annotations[0,:,:,f] = np.where(first_img == cell_id, relabel_id,
+                            relabeled_annotations[0,:,:,f])
+
+        # from that frame onwards, predict the rest
+        for frame in range(annotations.shape[0] -1):
+
+            # base labels off of this frame
+            img = relabeled_annotations[frame,:,:,f]
+
+            # this is frame that gets relabeled with predictions
+            next_img = annotations[frame + 1, :,:,f]
+
+            # put predictions into relabeled_annotations
+            predicted_next = predict_zstack_cell_ids(img, next_img, threshold)
+            relabeled_annotations[frame + 1, :,:,f] = predicted_next
+
+    # overwrite original npz with relabeled npz (only the annotations have changed)
+    np.savez(full_npz_path, X = raw, y = relabeled_annotations)
+
+    return None
+
+
+def relabel_npzs_folder(npz_dir, relabel_type = 'preserve', start_val = 1, threshold = 0.1):
+    '''
+    Relabel each npz in a folder with a given relabling strategy.
+    Relableling strategy is 'preserve' by default so that relationships
+    are not lost/scrambled unless user intentionally uses different strategy.
+    Note: "predict" relabeling scheme does not use deep learning predictions and
+    will not produce lineage predictions. This option is a computer vision approach
+    to help annotators when a deep learning prediction is not available.
+
+    Inputs:
+        npz_dir: full path to directory holding npzs to be relabeled. All npzs
+            in dir will be relabeled in place.
+        relabel_type: string to choose which relabeling function to call. Options
+            are:
+                preserve - preserve 3D relationships of labels
+                unique - each cell in each frame gets a unique label
+                all_frames - each frame is relabeled from start_val, independent of
+                    other frame labeling
+                predict - relabel first frame to start from 1, then predict labels in
+                    subsequent frames by iou comparisons between each frame and the next frame
+        start_val: which starting value to pass to relabeling function. 1 by default
+        threshold: iou between cells that must be reached for zstack prediction to
+            consider them matched. Default is 0.1, only used in relabel_npz_zstack_prediction
+
+    Returns:
+        None. Npzs are relabeled in place as function runs
     '''
 
     npz_list = list_npzs_folder(npz_dir)
@@ -406,7 +719,13 @@ def relabel_npzs_folder(npz_dir, relabel_type = 'unique'):
         full_npz_path = os.path.join(npz_dir, npz)
 
         if relabel_type == 'preserve':
-            pass
+            relabel_npz_preserve_relationships(full_npz_path, start_val)
 
         elif relabel_type == 'unique':
-            relabel_npz_unique(full_npz_path)
+            relabel_npz_unique(full_npz_path, start_val)
+
+        elif relabel_type == 'all_frames':
+            relabel_npz_all_frames(full_npz_path, start_val)
+
+        elif relabel_type == 'predict':
+            relabel_npz_zstack_prediction(full_npz_path, start_val, threshold)
