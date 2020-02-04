d588a03efc9153a825335cacde93600385eaf6ac
ngreenwald <noahfgreenwald@gmail.com>
2020-02-03 23:10:30 -0800

    removed unused modules from __init__ in utils

--- deepcell_toolbox/utils/__init__.py
+++ deepcell_toolbox/utils/__init__.py
@@ -29,12 +29,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from deepcell_toolbox.utils import dctb_data_utils
-#from deepcell_toolbox.utils import dctf_data_utils
-#from deepcell_toolbox.utils import retinanet_anchor_utils
+
 from deepcell_toolbox.utils import io_utils
 from deepcell_toolbox.utils import misc_utils
-from deepcell_toolbox.utils import transform_utils
 from deepcell_toolbox.utils import utils
 from deepcell_toolbox.utils import widget_utils
 
