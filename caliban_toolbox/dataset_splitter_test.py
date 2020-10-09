59a03e4183183e7b20b38056f51b6e493c6a25e0
ngreenwald <noahfgreenwald@gmail.com>
2020-10-09 08:22:15 -0700

    add option for splitting by image number

--- caliban_toolbox/dataset_splitter_test.py
+++ caliban_toolbox/dataset_splitter_test.py
@@ -31,34 +31,57 @@ from caliban_toolbox.dataset_splitter import DatasetSplitter
 
 
 def test__init__():
-    seed = 123,
-    splits = [0.5, 0.75, 1]
-
-    ds = DatasetSplitter(seed=seed, splits=splits)
-
+    seed = 123
+    ds = DatasetSplitter(seed=seed)
     assert ds.seed == seed
-    assert ds.splits == splits
 
-    # unsorted splits get sorted
-    splits = [0.8, 0.3, 0.5]
-    ds = DatasetSplitter(seed=seed, splits=splits)
 
-    assert ds.splits == sorted(ds.splits)
+def test__validate_split_counts():
+    # unsorted split_counts get sorted
+    ds = DatasetSplitter()
+    split_counts = [5, 1, 10]
+    valid_counts = ds._validate_split_counts(split_counts=split_counts)
 
-    with pytest.raises(ValueError):
-        # first split is size 0
-        splits = [0, 0.25, 0.5]
-        _ = DatasetSplitter(splits=splits)
+    assert valid_counts == sorted(valid_counts)
 
     with pytest.raises(ValueError):
-        # last split is greater than 1
-        splits = [0.1, 0.25, 1.5]
-        _ = DatasetSplitter(splits=splits)
+        # first split_count is size 0
+        split_counts = [0, 1, 4]
+        _ = ds._validate_split_counts(split_counts=split_counts)
 
     with pytest.raises(ValueError):
-        # duplicate splits
-        splits = [0.1, 0.1, 1]
-        _ = DatasetSplitter(splits=splits)
+        # duplicate split_counts
+        split_counts = [4, 8, 8]
+        _ = ds._validate_split_counts(split_counts=split_counts)
+
+    with pytest.raises(ValueError):
+        # non-integer split counts
+        split_counts = [4, 0.25, 7]
+        _ = ds._validate_split_counts(split_counts=split_counts)
+
+
+def test__validate_split_proportions():
+    # unsorted split_proportions get sorted
+    ds = DatasetSplitter()
+    split_proportions = [0.8, 0.3, 0.5]
+    valid_proportions = ds._validate_split_proportions(split_proportions=split_proportions)
+
+    assert valid_proportions == sorted(valid_proportions)
+
+    with pytest.raises(ValueError):
+        # first split_proportion is size 0
+        split_proportions = [0, 0.25, 0.5]
+        _ = ds._validate_split_proportions(split_proportions=split_proportions)
+
+    with pytest.raises(ValueError):
+        # last split_proportion is greater than 1
+        split_proportions = [0.1, 0.25, 1.5]
+        _ = ds._validate_split_proportions(split_proportions=split_proportions)
+
+    with pytest.raises(ValueError):
+        # duplicate split_proportions
+        split_proportions = [0.1, 0.1, 1]
+        _ = ds._validate_split_proportions(split_proportions=split_proportions)
 
 
 def test__validate_dict():
@@ -88,18 +111,18 @@ def test__duplicate_indices():
         assert set(test_idx) == set(duplicated_indices)
 
 
-def test_split():
+def test_split_by_proportion():
     X_vals = np.arange(100)
     y_vals = np.arange(100, 200)
 
     data_dict = {'X': X_vals, 'y': y_vals}
 
-    splits = [0.1, 0.5, 1]
-    ds = DatasetSplitter(splits=splits, seed=0)
-    split_dict = ds.split(train_dict=data_dict)
+    split_proportions = [0.1, 0.5, 1]
+    ds = DatasetSplitter(seed=0)
+    split_dict = ds.split(input_dict=data_dict, split_proportions=split_proportions)
 
     split_x_vals, split_y_vals = [], []
-    for split in splits:
+    for split in split_proportions:
         current_split = split_dict[str(split)]
 
         assert len(current_split['X']) == int(100 * split)
@@ -121,8 +144,8 @@ def test_split():
             split_y_vals = current_y_vals
 
     # same seed should produce same values
-    ds = DatasetSplitter(splits=splits, seed=0)
-    split_dict_same_seed = ds.split(train_dict=data_dict)
+    ds = DatasetSplitter(seed=0)
+    split_dict_same_seed = ds.split(input_dict=data_dict, split_proportions=split_proportions)
     for split in split_dict_same_seed:
         current_split = split_dict_same_seed[split]
         original_split = split_dict[split]
@@ -130,9 +153,9 @@ def test_split():
         for data in current_split:
             assert np.array_equal(current_split[data], original_split[data])
 
-    # differet seed should produce different values
-    ds = DatasetSplitter(splits=splits, seed=1)
-    split_dict_same_seed = ds.split(train_dict=data_dict)
+    # different seed should produce different values
+    ds = DatasetSplitter(seed=1)
+    split_dict_same_seed = ds.split(input_dict=data_dict, split_proportions=split_proportions)
     for split in split_dict_same_seed:
         current_split = split_dict_same_seed[split]
         original_split = split_dict[split]
@@ -141,11 +164,68 @@ def test_split():
             assert not np.array_equal(current_split[data], original_split[data])
 
     # split corresponding to fewer than 1 image returns a single image
-    splits = [0.001, 0.3, 1]
-    ds = DatasetSplitter(splits=splits, seed=0)
-    split_dict = ds.split(train_dict=data_dict)
+    split_proportions = [0.001, 0.3, 1]
+    ds = DatasetSplitter(seed=0)
+    split_dict = ds.split(input_dict=data_dict, split_proportions=split_proportions)
     assert len(split_dict['0.001']['X']) == 1
 
     # setting minimum size
-    split_dict = ds.split(train_dict=data_dict, min_size=10)
+    split_dict = ds.split(input_dict=data_dict, min_size=10, split_proportions=split_proportions)
     assert len(split_dict['0.001']['X']) == 10
+
+
+def test_split_by_count():
+    X_vals = np.arange(100)
+    y_vals = np.arange(100, 200)
+
+    data_dict = {'X': X_vals, 'y': y_vals}
+
+    split_counts = [4, 50, 100]
+    ds = DatasetSplitter(seed=0)
+    split_dict = ds.split(input_dict=data_dict, split_counts=split_counts)
+
+    split_x_vals, split_y_vals = [], []
+    for split in split_counts:
+        current_split = split_dict[str(split)]
+
+        assert len(current_split['X']) == split
+
+        if split_x_vals == []:
+            # first split
+            split_x_vals = current_split['X']
+            split_y_vals = current_split['y']
+        else:
+            # make sure all all previous values are in current split
+            current_x_vals = current_split['X']
+            current_y_vals = current_split['y']
+
+            assert np.all(np.isin(split_x_vals, current_x_vals))
+            assert np.all(np.isin(split_y_vals, current_y_vals))
+
+            # update counter with current values
+            split_x_vals = current_x_vals
+            split_y_vals = current_y_vals
+
+    # same seed should produce same values
+    ds = DatasetSplitter(seed=0)
+    split_dict_same_seed = ds.split(input_dict=data_dict, split_counts=split_counts)
+    for split in split_dict_same_seed:
+        current_split = split_dict_same_seed[split]
+        original_split = split_dict[split]
+
+        for data in current_split:
+            assert np.array_equal(current_split[data], original_split[data])
+
+    # different seed should produce different values
+    ds = DatasetSplitter(seed=1)
+    split_dict_same_seed = ds.split(input_dict=data_dict, split_counts=split_counts)
+    for split in split_dict_same_seed:
+        current_split = split_dict_same_seed[split]
+        original_split = split_dict[split]
+
+        for data in current_split:
+            assert not np.array_equal(current_split[data], original_split[data])
+
+    # setting minimum size
+    split_dict = ds.split(input_dict=data_dict, min_size=10, split_counts=split_counts)
+    assert len(split_dict['4']['X']) == 10
